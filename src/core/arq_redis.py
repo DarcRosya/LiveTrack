21f83acdeccbf87de5a16ecd04c1cac49335966f
Zero <darc.rosya@gmail.com>
2025-09-28 20:27:24 +0300

    +to previous commit

--- src/core/arq_redis.py
+++ src/core/arq_redis.py
@@ -3,28 +3,28 @@ from arq import create_pool
 from arq.connections import RedisSettings as ArqRedisSettings
 from src.config.settings import settings
 
-# Глобальная переменная для хранения пула соединений
+# Global variable for storing the connection pool
 arq_redis = None
 
 async def get_arq_redis():
-    """Зависимость для получения клиента ARQ."""
+    """Dependency for getting the ARQ client."""
     return arq_redis
 
 
 async def init_arq_redis():
-    """Инициализация пула соединений ARQ при старте приложения."""
+    """Initialize the ARQ connection pool at application startup."""
     global arq_redis
 
     arq_redis = await create_pool(
         ArqRedisSettings(
             host=settings.redis.HOST, 
             port=settings.redis.PORT, 
-            ssl=True,  # <-- Говорим, что нужно использовать SSL/TLS
-            ssl_ca_certs=settings.redis.SSL_CA_CERTS # <-- И указываем путь к сертификату
+            ssl=True,  # <-- Specify that SSL/TLS should be used
+            ssl_ca_certs=settings.redis.SSL_CA_CERTS  # <-- Provide the path to the certificate
         )
     )
 
 
 async def close_arq_redis():
-    """Закрытие пула соединений при остановке приложения."""
+    """Close the connection pool when stopping the application."""
     await arq_redis.close()
